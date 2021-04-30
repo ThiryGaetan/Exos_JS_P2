d06a55ee9cf65cdcf29ef10dab6e71b784484044
ThiryGaetan <gaetan.thiry@outlook.fr>
2021-04-30 16:27:27 +0200

    bg-color ex3

--- Exercice3/assets/js/script.js
+++ Exercice3/assets/js/script.js
@@ -3,8 +3,8 @@ let username = 'gaetan';
 input.addEventListener('input', checkInput);
 function checkInput() {
     if(input.value == username){
-        document.querySelector('input').className = 'is-valid';
+        document.querySelector('input').className = 'is-valid bg-success';
     }else{
-        document.querySelector('input').className = 'is-invalid';
+        document.querySelector('input').className = 'is-invalid bg-danger';
     }
 }
