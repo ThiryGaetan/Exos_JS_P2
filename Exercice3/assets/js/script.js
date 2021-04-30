4929c965613878add2041defdffc3bdbedf666d8
ThiryGaetan <gaetan.thiry@outlook.fr>
2021-04-30 15:47:18 +0200

    fix function

--- Exercice3/assets/js/script.js
+++ Exercice3/assets/js/script.js
@@ -1,7 +1,7 @@
 let input = document.querySelector('input');
 let username = 'gaetan';
-input.addEventListener('input', z);
-function z() {
+input.addEventListener('input', checkInput);
+function checkInput() {
     if(input.value == username){
         document.querySelector('input').className = 'is-valid';
     }else{
